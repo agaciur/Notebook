5881c1053d4acadd13724d51906b0c8b02b334cb
Agata Rojewska <agata.rojewska@gmail.com>
2024-02-27 20:02:42 +0100

    Add posiibility to hide bar

--- src/components/Notebooks/Notebooks.jsx
+++ src/components/Notebooks/Notebooks.jsx
@@ -1,33 +1,56 @@
 import styles from "./Notebooks.module.css"
 import { Notebook } from "../Notebook/Notebook"
 import ARCHIVE_ICON from "../../assets/archive.svg"
+import ARROW_TRIANGLE_ICON from "../../assets/arrow-triangle.svg"
 import { NavLink } from "react-router-dom"
 import { InputWithAddButton } from "../InputWithAddButton/InputWithAddButton"
+import { useState } from "react"
 export function Notebooks({ notebooks }) {
+  const [hideBar, setHideBar] = useState(false)
+
   return (
     <>
-      <div className={styles.notebooks}>
-        <div>
-          <h3>Dodaj notatnik:</h3>
-          <InputWithAddButton placeholder={"nazwa natatnika"} />
-          <h3>Notatniki:</h3>
-          {notebooks.map(notebook => (
-            <NavLink
-              key={notebook.id}
-              to={`/notes/${notebook.id}`}>
-              {({ isActive }) => {
-                return <Notebook active={isActive}>{notebook.title}</Notebook>
-              }}
-            </NavLink>
-          ))}
-        </div>
-        <div className={styles.archive}>
-          <button>
-            <img src={ARCHIVE_ICON} />
-            <p>Archiwum</p>
+      {hideBar ? (
+        <div className={styles.hideBar}>
+          <button
+            className={styles.buttonArray}
+            onClick={() => setHideBar(false)}>
+            <img src={ARROW_TRIANGLE_ICON} />
           </button>
+          <p>Notatniki</p>
         </div>
-      </div>
+      ) : (
+        <div className={styles.notebooks}>
+          <div>
+            <div className={styles.arrowIcon}>
+              <button
+                onClick={() => setHideBar(true)}
+                className={styles.buttonArray}>
+                <img src={ARROW_TRIANGLE_ICON} />
+              </button>
+            </div>
+
+            <h3>Dodaj notatnik:</h3>
+            <InputWithAddButton placeholder={"nazwa natatnika"} />
+            <h3>Notatniki:</h3>
+            {notebooks.map(notebook => (
+              <NavLink
+                key={notebook.id}
+                to={`/notes/${notebook.id}`}>
+                {({ isActive }) => {
+                  return <Notebook active={isActive}>{notebook.title}</Notebook>
+                }}
+              </NavLink>
+            ))}
+          </div>
+          <div className={styles.archive}>
+            <button>
+              <img src={ARCHIVE_ICON} />
+              <p>Archiwum</p>
+            </button>
+          </div>
+        </div>
+      )}
     </>
   )
 }
--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -4,11 +4,13 @@ import ICON_NOTE from "../../assets/note.svg"
 import ADD_ICON from "../../assets/add.svg"
 import { NavLink, useParams, Outlet, useLoaderData } from "react-router-dom"
 import { notes } from "../../data/notes"
+import ARROW_TRIANGLE_ICON from "../../assets/arrow-triangle.svg"
 import { ShortNote } from "../ShortNote/ShortNote"
 import { InputWithAddButton } from "../InputWithAddButton/InputWithAddButton"
 export function NotesList() {
   const { folderId, noteId } = useParams()
   let notes = useLoaderData()
+  const [hideBar, setHideBar] = useState(false)
 
   const [notesList, setNotesList] = useState(() => {
     notes.filter(book => book.id === Number(folderId))
@@ -59,25 +61,48 @@ export function NotesList() {
         </>
       ) : (
         <>
-          <div className={styles.noteslist}>
-            <h3>Dodaj notatkę:</h3>
-            <InputWithAddButton placeholder={"Nazwa notatki"} />
-            <h3>Notatki:</h3>
-            <ul>
-              {notesList.map(note => (
-                <NavLink
-                  key={note.id}
-                  to={`/notes/${folderId}/note/${note.id}`}>
-                  {({ isActive }) => (
-                    <ShortNote
-                      active={isActive}
-                      note={note}></ShortNote>
-                  )}
-                </NavLink>
-              ))}
-            </ul>
-          </div>
-          <Outlet />
+          {hideBar ? (
+            <>
+              <div className={styles.hideBar}>
+                <button
+                  className={styles.buttonArray}
+                  onClick={() => setHideBar(false)}>
+                  <img src={ARROW_TRIANGLE_ICON} />
+                </button>
+                <p>Notatki</p>
+              </div>
+              <Outlet />
+            </>
+          ) : (
+            <>
+              <div className={styles.noteslist}>
+                <div className={styles.arrowIcon}>
+                  <button
+                    onClick={() => setHideBar(true)}
+                    className={styles.buttonArray}>
+                    <img src={ARROW_TRIANGLE_ICON} />
+                  </button>
+                </div>
+                <h3>Dodaj notatkę:</h3>
+                <InputWithAddButton placeholder={"Nazwa notatki"} />
+                <h3>Notatki:</h3>
+                <ul>
+                  {notesList.map(note => (
+                    <NavLink
+                      key={note.id}
+                      to={`/notes/${folderId}/note/${note.id}`}>
+                      {({ isActive }) => (
+                        <ShortNote
+                          active={isActive}
+                          note={note}></ShortNote>
+                      )}
+                    </NavLink>
+                  ))}
+                </ul>
+              </div>
+              <Outlet />
+            </>
+          )}
         </>
       )}
     </>
