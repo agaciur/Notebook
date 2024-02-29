df730ec5530fff12033a4ddbfdcfe895802a121d
Agata Rojewska <agata.rojewska@gmail.com>
2024-02-29 18:00:35 +0100

    Fix some styles

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,26 @@
 import styles from "./Logo.module.css"
 import PEN_ICON from "../../assets/pen.svg"
+import LOGOUT_ICON from "../../assets/log-out.svg"
+import ACCOUNT_ICON from "../../assets/account.svg"
 export function Logo() {
+  const handleLogout = () => {
+    localStorage.removeItem("jwtToken")
+  }
+
   return (
     <div className={styles.logo}>
-      <img src={PEN_ICON} />
-      <h1>Notes</h1>
+      <div>
+        <img src={PEN_ICON} />
+        <h1>Notes</h1>
+      </div>
+      <div>
+        <div>
+          <img
+            className={styles.logout}
+            src={ACCOUNT_ICON}
+          />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -15,27 +15,31 @@ export function Note() {
 
   return (
     <>
-      <Editor
-        apiKey='srlddd1qxwh6gmrys6jtp47e5j4n0hl3g2p1c0i3g3hv9e60'
-        key={note.id}
-        onEditorChange={handleEditorChange}
-        init={{
-          spellchecker_language: "pl",
-          plugins:
-            "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss",
-          toolbar:
-            "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-          tinycomments_mode: "embedded",
-          tinycomments_author: "Author name",
-          mergetags_list: [
-            { value: "First.Name", title: "First Name" },
-            { value: "Email", title: "Email" },
-          ],
-          ai_request: (request, respondWith) =>
-            respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-        }}
-        initialValue={`<h3>${note.title}</h3> <p>${note.content}</p>`}
-      />
+      <div className={styles.note}>
+        <Editor
+          apiKey='srlddd1qxwh6gmrys6jtp47e5j4n0hl3g2p1c0i3g3hv9e60'
+          key={note.id}
+          onEditorChange={handleEditorChange}
+          init={{
+            spellchecker_language: "pl",
+            selector: "textarea",
+            height: "100%",
+            plugins:
+              "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss",
+            toolbar:
+              "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+            tinycomments_mode: "embedded",
+            tinycomments_author: "Author name",
+            mergetags_list: [
+              { value: "First.Name", title: "First Name" },
+              { value: "Email", title: "Email" },
+            ],
+            ai_request: (request, respondWith) =>
+              respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+          }}
+          initialValue={`<h3>${note.title}</h3> <p>${note.content}</p>`}
+        />
+      </div>
     </>
   )
 }
