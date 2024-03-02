1bb04a7fc964b5133e792a107770d6e1661468b8
Agata Rojewska <agata.rojewska@gmail.com>
2024-03-02 18:37:02 +0100

    Add logout

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,43 @@
 import styles from "./Logo.module.css"
 import PEN_ICON from "../../assets/pen.svg"
-import LOGOUT_ICON from "../../assets/log-out.svg"
 import ACCOUNT_ICON from "../../assets/account.svg"
+import SETTING_ICON from "../../assets/settings.svg"
+import LOGOUT_ICON from "../../assets/log-out.svg"
+import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
+import { useState, useRef, useEffect } from "react"
+
 export function Logo() {
+  const [clickAccount, setClickAccount] = useState(false)
+  const buttonRef = useRef(null)
+  const ulRef = useRef(null)
+
   const handleLogout = () => {
     localStorage.removeItem("jwtToken")
+    window.location.href = "/zaloguj"
   }
+
+  const handleClick = () => {
+    setClickAccount(!clickAccount)
+  }
+
+  const handleClickOutside = event => {
+    if (
+      buttonRef.current &&
+      !buttonRef.current.contains(event.target) &&
+      ulRef.current &&
+      !ulRef.current.contains(event.target)
+    ) {
+      setClickAccount(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
 
   return (
     <div className={styles.logo}>
@@ -13,13 +45,36 @@ export function Logo() {
         <img src={PEN_ICON} />
         <h1>Notes</h1>
       </div>
+
       <div>
-        <div>
+        <motion.button
+          ref={buttonRef}
+          whileTap={{ scale: 0.9 }}
+          onClick={handleClick}>
           <img
             className={styles.logout}
             src={ACCOUNT_ICON}
           />
-        </div>
+        </motion.button>
+
+        {clickAccount && (
+          <ul
+            className={styles.account}
+            ref={ulRef}>
+            <li>
+              <Link>
+                <img src={SETTING_ICON} />
+                <span>Ustawienia</span>
+              </Link>
+            </li>
+            <li>
+              <button onClick={handleLogout}>
+                <img src={LOGOUT_ICON} />
+                <span>Wyloguj</span>
+              </button>
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   )
